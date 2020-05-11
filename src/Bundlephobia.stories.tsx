67c99d315a9a0f920b6d516e60022a3f8b3241f8
Nate Radebaugh <nate.radebaugh@outlook.com>
2020-05-10 20:34:11 -0500

    Add knobs to storybook to try out different package names

--- src/Bundlephobia.stories.tsx
+++ src/Bundlephobia.stories.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Bundlephobia, useBundlephobia } from ".";
+import { withKnobs, text as textKnob } from "@storybook/addon-knobs";
+import { BundlephobiaInline, useBundlephobia } from "./index";
 
 export default {
-  title: "Bundlephobia"
+  title: "Bundlephobia",
+  decorators: [withKnobs]
 };
 
 export function AsHook() {
+  const packageName = textKnob("packageName", "react-query");
+
   // Simple wrapper for react-query + the bundlephobia API
-  const response = useBundlephobia("react-query");
+  const response = useBundlephobia(packageName);
   const { status } = response;
   const data: any = response.data;
 
@@ -32,5 +36,7 @@ export function AsHook() {
 }
 
 export function AsComponent() {
-  return <Bundlephobia packageName="react-query" />;
+  const packageName = textKnob("packageName", "react-query");
+
+  return <BundlephobiaInline packageName={packageName} />;
 }
